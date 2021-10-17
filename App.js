5204d7ee72d82162e2407c6dfe2d5c73d3419cd2
Philip Kabranov <Kabranov12@gmail.com>
2021-10-17 13:22:06 -0700

    added map

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
+import { Appbar, Button, Card, Paragraph } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import { COLOR_ACCENT, COLOR_PRIMARY } from "./AppStyles";
+
+// Theme Object for React Native Paper
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: COLOR_PRIMARY,
+    accent: COLOR_ACCENT,
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Modified</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: 37.8719,
+              longitude: -122.2585,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            <Marker
+              key={1}
+              coordinate={{ latitude: 37.8719, longitude: -122.2585 }}
+              title={"UC Berkeley"}
+              description={"#1 Public University"}
+            />
+          </MapView>
+          <Appbar
+            title="button"
+            style={{
+              position: "absolute",
+              bottom: 50,
+              width: Dimensions.get("window").width,
+            }}
+          />
+        </View>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
